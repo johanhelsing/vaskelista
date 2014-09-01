0296a86e102fcd4fbcc2d5c4ee4cfa28bfe3bc6a
Johan Helsing <johan.helsing@bekk.no>
2014-09-01 14:31:58 +0200

    Add Room model

---
+++ Vaskelista/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vaskelista.Models
+{
+    public class Room
+    {
+        public Int32 RoomId { get; set; }
+
+        [Required]
+        [Display(Name = "Navn")]
+        public string Name { get; set; }
+
+        [Display(Name = "Beskrivelse")]
+        public string Description { get; set; }
+    }
+}
