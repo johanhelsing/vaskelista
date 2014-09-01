bd92e5cc8023d220e5ce60a9706432416a52f068
Johan Helsing <johan.helsing@bekk.no>
2014-09-01 13:31:20 +0200

    Add Visual Studio asp.net MVC template
    Template for MVC internet application including user authentication and Web API

---
+++ Vaskelista/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vaskelista.MvcApplication" Language="C#" %>
---
+++ Vaskelista/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vaskelista.Startup))]
+namespace Vaskelista
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
