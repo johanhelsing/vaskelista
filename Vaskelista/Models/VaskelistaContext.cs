f6228e4535d5f327613ad87aceecd3ae77994b46
Johan Helsing <johan.helsing@bekk.no>
2014-09-01 14:44:06 +0200

    Add DbContext, VaskelistaContext

---
+++ Vaskelista/Models/VaskelistaContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vaskelista.Models
+{
+    public class VaskelistaContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
