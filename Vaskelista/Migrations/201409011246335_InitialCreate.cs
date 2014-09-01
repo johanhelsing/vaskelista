4e45831c1a944a79953e1bf6c4ace55b5a90da35
Johan Helsing <johan.helsing@bekk.no>
2014-09-01 15:51:50 +0200

    Enable database migrations

---
+++ Vaskelista/Migrations/201409011246335_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace Vaskelista.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Rooms",
+                c => new
+                    {
+                        RoomId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.RoomId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Rooms");
+        }
+    }
+}
---
+++ Vaskelista/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Vaskelista.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Vaskelista.Models.VaskelistaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Vaskelista.Models.VaskelistaContext";
+        }
+
+        protected override void Seed(Vaskelista.Models.VaskelistaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
