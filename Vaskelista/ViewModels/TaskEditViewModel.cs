b4294ea2021513a8e23b110905f5e20d05808c2f
Johan Helsing <johan.helsing@bekk.no>
2014-09-09 14:30:43 +0200

    Add stub for TaskEditViewModel

---
+++ Vaskelista/ViewModels/TaskEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vaskelista.ViewModels
+{
+    public class TaskEditViewModel : TaskCreateViewModel
+    {
+        public Int32 TaskId { get; set; }
+    }
+}
